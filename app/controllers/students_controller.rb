3b16846379c39b93f2aa505007d7dc5dd4d12b9e
Scout Cox <coxscout@gmail.com>
2022-11-02 11:58:09 -0400

    commit tree cf562ed2d360e8895ba4a2aea7da4fa3146f7442

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,18 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def index
+    students = if params[:name]
+                Student.by_name(params[:name])
+              else
+                Student.all
+              end    
+    render json: students
+  end
+
 end
